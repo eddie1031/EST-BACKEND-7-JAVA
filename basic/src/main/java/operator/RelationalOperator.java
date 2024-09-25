6583a2aea258281de961b6bb9af28a5f53df60dc
eddie1031 <archaco123@gmail.com>
2024-09-25 23:50:39 +0900

    9강 - 연산자, 관계연산자

---
+++ basic/src/main/java/operator/RelationalOperator.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class RelationalOperator {
+
+    public static void main(String[] args) {
+
+        // # 관계 연산자
+        int big = 10;
+        int small = 5;
+
+        System.out.println(big > small); // true
+        System.out.println(big < small); // false
+        System.out.println(big <= small); // false
+        System.out.println(big >= small); // true
+        System.out.println(big == small); // false
+        System.out.println(big != small); // true
+
+    }
+
+}
