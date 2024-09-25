ae35b3d06acc799a66a6e9cc703304b74206fc07
eddie1031 <archaco123@gmail.com>
2024-09-25 23:09:47 +0900

    2강 - 변수의 선언과 할당, 초기화

---
+++ basic/src/main/java/variable/Variable1.java
@@ -0,0 +1,26 @@
+package variable;
+
+public class Variable1 {
+
+    public static void main(String[] args) {
+
+        // 선언(Declare)
+        int integerVariable;
+        // 할당(Allocate)
+        integerVariable = 10;
+
+        // 초기화(Initialize)
+        char characterVariable = 'A';
+
+        // 같은 타입의 여러 변수 선언
+        int age, height, weight;
+        age = 10;
+        height = 20;
+        weight = 30;
+
+        // 같은 타입의 여러 변수 초기화
+        int w = 1980, h = 1080;
+
+    }
+
+}
