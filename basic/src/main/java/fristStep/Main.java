61efc7d8737c416401389b735523114cb7352cfc
eddie1031 <archaco123@gmail.com>
2024-09-25 23:04:22 +0900

    1강 - Hello, World!

---
+++ basic/src/main/java/fristStep/Main.java
@@ -0,0 +1,10 @@
+package fristStep;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("안녕, 세상아!");
+        System.out.println("Hello World!");
+    }
+
+}
