1f0c949aa1ebd027543c35a5ad6a61b2911f15d6
eddie1031 <archaco123@gmail.com>
2024-09-27 14:58:26 +0900

    22강 - 함수, 매개변수가 있고 반환이 없는 함수

---
+++ basic/src/main/java/methods/Method4.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class Method4 {
+
+    public static void main(String[] args) {
+
+        buyIceCream(500);
+
+    }
+
+    static void buyIceCream(int money) {
+        System.out.println(money + "원 을 사용해서 아이스크림을 사왔습니다.");
+    }
+
+}
