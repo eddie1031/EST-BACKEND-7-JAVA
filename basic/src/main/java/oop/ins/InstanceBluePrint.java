b31ef7da47ddc345a75ea8ab71096de4a4527cff
eddie1031 <archaco123@gmail.com>
2024-09-30 16:07:07 +0900

    29강 - OOP, 인스턴스 이해

---
+++ basic/src/main/java/oop/ins/InstanceBluePrint.java
@@ -0,0 +1,17 @@
+package oop.ins;
+
+public class InstanceBluePrint {
+
+    public String field1 = "field1";
+    public int field2 = 1;
+
+    public void method1() {
+        System.out.println("InstanceBluePrint.method1");
+    }
+
+    public void method2() {
+        System.out.println("field1 = " + field1);
+        System.out.println("field2 = " + field2);
+    }
+
+}
---
+++ basic/src/main/java/oop/ins/InstanceBluePrintTests.java
@@ -0,0 +1,46 @@
+package oop.ins;
+
+public class InstanceBluePrintTests {
+
+    public static void main(String[] args) {
+
+        test1();
+        test2();
+
+    }
+
+    private static void test1() {
+        InstanceBluePrint instance = new InstanceBluePrint();
+
+        String insField1 = instance.field1;
+        int insField2 = instance.field2;
+
+        System.out.println("insField1 = " + insField1);
+        System.out.println("insField2 = " + insField2);
+
+        instance.method1();
+        instance.method2();
+
+        instance.field1 = "InjectedValue";
+        instance.field2 = 999;
+
+        System.out.println("insField1 = " + insField1);
+        System.out.println("insField2 = " + insField2);
+
+        instance.method1();
+        instance.method2();
+    }
+
+    static void test2() {
+
+        InstanceBluePrint instance1 = new InstanceBluePrint();
+        InstanceBluePrint instance2 = new InstanceBluePrint();
+
+        instance1.field1 = "InjectedValue";
+
+        instance2.method2(); // ? ?
+        instance1.method2(); // ? ?
+
+    }
+
+}
